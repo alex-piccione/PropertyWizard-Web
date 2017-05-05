2b372f4a2ad3d47685291593be9558bd3aceaab2
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-05 01:48:01 +0100

    Add class Listing.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
 
+export class Listing {
+    id: number;
+    postcode: string;
+}
+
 @Component({
     selector: "pw-app",
-    template: `<h1>Component {{name}}</h1>`
+    template: `<h1>Component: {{title}}. Listing: {{listing.id}}, postcode: {{listing.postcode}}.</h1>`
 })
 
-export class AppComponent { name = "Angular"; }
+export class AppComponent { 
+    title = "Listing";
+    listing: Listing = {
+        id: 3,
+        postcode: 'aaa'
+    };
+}
