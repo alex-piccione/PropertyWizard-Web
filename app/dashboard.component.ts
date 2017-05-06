6c3eab005845e36cd1a18f74acafe8895044084b
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-06 17:32:24 +0100

    Load some postcodes in the dashboard.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { PostcodeService } from "./postcode.service";
+import { Postcode } from "./postcode";
+
 
 @Component({
     selector: "pw-dashboard",
     templateUrl: "/templates/dashboard.template.html"
 })
 
-export class DashboardComponent {}
+export class DashboardComponent {
+    postcodes: Postcode[] = [];
+
+    constructor(private postcodeService: PostcodeService) { }
+
+    ngOnInit(): void {
+        this.postcodeService.getPostcodes().then(postcodes =>
+            this.postcodes = postcodes.slice(1, 3)
+        );        
+    }
+}
