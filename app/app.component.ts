fe83010d06c5ddf3864b36b5b9009c76d13a87bd
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-06 13:52:50 +0100

    Add a main component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,83 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { Postcode} from "./Postcode";
-import { PostcodeService } from "./postcode.service";
+import { Component } from "@angular/core";
 
-@Component({    
+
+@Component(
+{
     selector: "pw-app",
-    template: `<h1>{{title}}</h1>
-        <ul class="postcodes">
-            <li *ngFor="let postcode of postcodes" 
-                (click)="onSelect(postcode)" 
-                [class.selected]="postcode === selectedPostcode">
-                <span class="badge">{{postcode.code}}</span> {{postcode.description}}
-            </li>
-        </ul>
-        <postcode-detail [postcode]="selectedPostcode"></postcode-detail>
-    `,
-    styles: [`
-        .selected { background-color: #cfd8dc !important; color: white; }
-        .postcodes { 
-            margin: 0 0 2em 0;
-            list-style-type: none;
-            padding: 0;
-            width: 15em;
-        }
-        .postcodes li {
-            cursor: pointer;
-            position: relative;
-            left: 0;
-            background-color: #eee;
-            margin: .5em;
-            padding: .3em 0;
-            height: 1.6em;
-            border-radius: 4px;
-        }
-        .postcodes li:selected:hover {
-            background-color: #bbd8dc !important;
-            color: white;
-        }
-        .postcodes li:hover {
-            color: #607d8b;
-            background-color: #ddd;
-            left: .1em;
-        }
-        .postcodes .text { position: relative; top: -3px; }
-        .postcodes .badge {
-            display: inline-block;
-            font-size: small;
-            color: white;
-            padding: 0.8em 0.7em 0 0.7em;
-            background-color: #607d8b;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -4px;
-            height: 1.8em;
-            margin-right: .8em;
-            border-radius: 4px 0 0 4px;
-        }
-    `],
-    providers: [ PostcodeService ]
+    template: `
+    <h1>{{title}}</h1>
+    <pw-postcodes></pw-postcodes>
+    `
 })
 
-export class AppComponent { 
-    title: string = "Listings";
-    postcodes: Postcode[];
-    selectedPostcode: Postcode;
-
-    constructor(private postcodeService: PostcodeService) { }
-
-    ngOnInit(): void {        
-        this.loadPostcodes();
-    }
-
-    onSelect(postcode: Postcode): void {
-        this.selectedPostcode = postcode;
-    }     
-
-    loadPostcodes(): void {
-        this.postcodeService.getPostcodes().then(postcodes =>
-            this.postcodes = postcodes
-        );        
-    }
+export class AppComponent {
+    title: "Listings"
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
+import { PostcodesComponent } from "./postcodes.component";
 import { PostcodeDetailComponent} from "./postcode-detail.component";
 
 @NgModule({
     imports: [ BrowserModule, FormsModule ],
     declarations: [ 
         AppComponent, 
+        PostcodesComponent,
         PostcodeDetailComponent 
     ],
     bootstrap: [ AppComponent ]
