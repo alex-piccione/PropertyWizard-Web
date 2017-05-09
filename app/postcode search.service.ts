03df3c1b7616e9576250bab0a5e99f950ebd3291
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-09 18:32:04 +0100

    Add component search service.

---
+++ app/postcode search.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+
+import { Observable } from "rxjs/observable";
+import "rxjs/add/operator/map"
+
+import { Postcode } from "./Postcode";
+
+Injectable()
+export class PostcodeSearchService { 
+
+    constructor(private http: Http) {}
+
+    search(term: string): Observable<Postcode[]> {
+        return this.http.get(`app/postcodes/?name=${term}`)
+            .map(response => response.json().data as Postcode[]);
+    }
+}
