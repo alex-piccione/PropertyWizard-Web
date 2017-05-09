466435f7f90ba86d4ec32c648048cad533092d3b
Alex75 <alessandro.piccione.75@gmail.com>
2017-05-09 20:36:07 +0100

    Add a textbox and a list of result to the postcode search component. Set the placeholder for the search and gotoDetail methods. Set the CSS styles.

--- app/postcode search.component.ts
+++ app/postcode search.component.ts
@@ -1,7 +1,19 @@
-import { Component, Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+//import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
 
+import { Postcode } from "./postcode";
 import { PostcodeSearchService } from "./postcode search.service";
+
+// Observable class extension
+import "rxjs/add/observable/of";
+
+// Observale operators
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/distinctUntilChanged";
 
 @Component({
     selector: "postcode-search",
@@ -9,11 +21,42 @@ import { PostcodeSearchService } from "./postcode search.service";
     styleUrls: ["templates/postcode search.css"]
 })
 
-@Injectable()
-export class PostcodeSearchComponent {
+//@Injectable()
+export class PostcodeSearchComponent implements OnInit {
 
-    constructor(private postcodeSearchService: PostcodeSearchService)
+    postcodes: Observable<Postcode[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(
+        private postcodeSearchService: PostcodeSearchService,
+        private router: Router    
+    )
     {
-
     }    
+
+    ngOnInit(): void {
+        this.postcodes = this.searchTerms
+            .debounceTime(300) // wait 300ms before each keystroke before considering the term
+            .distinctUntilChanged() // ignore if search tesrm is same as previous
+            .switchMap(term => term // switch to new observable everytime the term changes
+            // return the http search observable
+            ? this.postcodeSearchService.search(term)
+            // or the observable of empty ostcodes if there was no search term
+            : Observable.of<Postcode[]>([])
+            .catch(error => {
+                // todo: add real error handling
+                console.log(error );
+                return Observable.of<Postcode[]>([]);
+            }));
+    }
+
+    // push a search term into the strem
+    search(term: string): void {
+        this.searchTerms.next(term);
+    }
+
+    gotoDetail(postcode: Postcode): void {
+        let link = ["/detail", postcode.id];
+        this.router.navigate(link);
+    }
 }
