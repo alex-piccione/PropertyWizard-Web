5018b1c354fdd7c790965414cb3704e9bc310a40
Alex75 <alessandro.piccione.75@gmail.com>
2017-06-06 22:12:29 +0100

    Change batch to launch development server to check for building errors.

--- start dev server.bat
+++ start dev server.bat
@@ -1,4 +1,25 @@
 :: if no "script" is defined in package.json it will run node server.js
+
+:: this one ignore errors on build !
+::npm run start_dev
+
+call npm run build
+
+IF %ERRORLEVEL% neq 0 GOTO :ERROR
+
+:: no error, run it
 npm run start_dev
 
+GOTO END
+
+:ERROR
+
+echo.
+echo.
+echo Error level for build is "%ERRORLEVEL%".
+echo Check the error(s).
+echo.
+
 pause
+
+:END
