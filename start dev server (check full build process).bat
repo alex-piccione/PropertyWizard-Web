4a86af2452f7f38a2dc122e8d7bf9a0eca6bbd7a
Alex75 <alessandro.piccione.75@gmail.com>
2017-07-08 21:40:46 +0100

    Add useful Windows batch file.

---
+++ start dev server (check full build process).bat
@@ -0,0 +1,30 @@
+:: if no "script" is defined in package.json it will run node server.js
+
+:: this one ignore errors on build !
+::npm run start_dev
+
+call npm run clean_windows
+:: check for build errors
+call npm run build  
+
+IF %ERRORLEVEL% neq 0 GOTO :ERROR
+
+:: no error, continue
+call npm run bundle 
+call npm run minify
+npm run start_server
+
+
+GOTO END
+
+:ERROR
+
+echo.
+echo.
+echo Error level for build is "%ERRORLEVEL%".
+echo Check the error(s).
+echo.
+
+pause
+
+:END
